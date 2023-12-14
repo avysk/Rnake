bb25a46fee4ee4241df11ae0296c765ef40c77ce
Alexey Vyskubov <alexey@ocaml.nl>
2023-12-14 17:35:44 +0200

    First working (but minimal) version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,148 @@
-fn main() {
-    println!("Hello, world!");
+mod world;
+use std::cmp::min;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::sys::{SDL_Delay, SDL_GetTicks64, Uint32, Uint64};
+
+use crate::world::{Thing, World, HEIGHT, WIDTH};
+
+const FRAME_DELTA: Uint64 = 60;
+// update screen after the given number of SDL ticks
+const WAIT: Uint64 = 20;
+
+const WIN_WIDTH: u32 = 600;
+const WIN_HEIGHT: u32 = 600;
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rnake", WIN_WIDTH, WIN_HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    let mut w = World::init();
+    let cell_width = (WIN_WIDTH as i32) / (WIDTH as i32);
+    let cell_height = (WIN_HEIGHT as i32) / (HEIGHT as i32);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut next_frame: Uint64 = 0;
+    let mut turned = false;
+    'running: loop {
+        // process quit and turn the snake events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    if !turned {
+                        w.turn_right();
+                        turned = true;
+                    }
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    if !turned {
+                        w.turn_left();
+                        turned = true;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        // check if we are at the right moment
+        unsafe {
+            if SDL_GetTicks64() < next_frame {
+                SDL_Delay(min(
+                    WAIT as Uint32,
+                    (next_frame - SDL_GetTicks64()) as Uint32,
+                ));
+                continue 'running;
+            }
+        }
+
+        // Advance
+        if w.step().is_err() {
+            break 'running;
+        }
+
+        // Clear the field
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+
+        // draw the snake head
+        canvas.set_draw_color(Color::GREEN);
+        let (hx, hy) = w
+            .snake
+            .first()
+            .expect("Programming error: a snake cannot be empty");
+        let r = Rect::new(
+            cell_width * (*hx as i32),
+            cell_height * (*hy as i32),
+            cell_width as u32,
+            cell_height as u32,
+        );
+        canvas
+            .fill_rect(r)
+            .expect("SDL error: cannot draw rectangle");
+
+        // draw rest of the snake
+        canvas.set_draw_color(Color::GRAY);
+        for (x, y) in &w.snake[1..] {
+            let r = Rect::new(
+                cell_width * (*x as i32),
+                cell_height * (*y as i32),
+                cell_width as u32,
+                cell_height as u32,
+            );
+            canvas
+                .fill_rect(r)
+                .expect("SDL error: cannot draw rectangle");
+        }
+
+        // Draw the things
+        for (t, x, y) in &(w.things) {
+            let r = Rect::new(
+                cell_width * (*x as i32),
+                cell_height * (*y as i32),
+                cell_width as u32,
+                cell_height as u32,
+            );
+            let c = match t {
+                Thing::Food => Color::BLUE,
+            };
+            canvas.set_draw_color(c);
+            canvas
+                .fill_rect(r)
+                .expect("SDL error: cannot draw rectangle");
+        }
+
+        canvas.present();
+
+        next_frame += FRAME_DELTA;
+        turned = false;
+
+        if w.things.is_empty() {
+            w.add_thing();
+        }
+    }
 }
+
---
+++ src/world.rs
@@ -0,0 +1,138 @@
+use anyhow::{bail, Result};
+use rand::Rng;
+
+pub const WIDTH: usize = 45;
+pub const HEIGHT: usize = 45;
+
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+const SNAKE_INIT_X: usize = WIDTH / 2;
+const SNAKE_INIT_Y: usize = HEIGHT / 2;
+const SNAKE_INIT_DIR: Direction = Direction::Up;
+
+#[derive(PartialEq)]
+pub enum Thing {
+    Food,
+}
+
+pub struct World {
+    pub snake: Vec<(usize, usize)>,
+    snake_dir: Direction,
+    pub things: Vec<(Thing, usize, usize)>,
+    grow: i32, // grow for this amount of turns; 0 means do not grow
+}
+
+impl World {
+    pub fn init() -> Self {
+        World {
+            snake: vec![
+                (SNAKE_INIT_X, SNAKE_INIT_Y),
+                (SNAKE_INIT_X, SNAKE_INIT_Y + 1),
+                (SNAKE_INIT_X, SNAKE_INIT_Y + 2),
+            ],
+            snake_dir: SNAKE_INIT_DIR,
+            things: vec![],
+            grow: 0,
+        }
+    }
+    pub fn step(&mut self) -> Result<()> {
+        let (mut next_x, mut next_y) = self.snake[0];
+        match self.snake_dir {
+            Direction::Up => {
+                if next_y > 0 {
+                    next_y -= 1;
+                } else {
+                    bail!("Snake went out of the field")
+                }
+            }
+            Direction::Down => {
+                if next_y < HEIGHT {
+                    next_y += 1
+                } else {
+                    bail!("Snake went out of the field")
+                }
+            }
+            Direction::Right => {
+                if next_x > 0 {
+                    next_x -= 1;
+                } else {
+                    bail!("Snake went out of the field")
+                }
+            }
+            Direction::Left => {
+                if next_x < WIDTH {
+                    next_x += 1
+                } else {
+                    bail!("Snake went out of the field")
+                }
+            }
+        };
+        // Check if we ate something
+        let idx = self
+            .things
+            .iter()
+            .position(|x| x.1 == next_x && x.2 == next_y);
+        if let Some(pos) = idx {
+            match self.things[pos].0 {
+                Thing::Food => self.grow += 3,
+            };
+            self.things.swap_remove(pos);
+        }
+        // Maybe shrink snake
+        if self.grow == 0 {
+            self.snake.pop();
+        } else {
+            assert!(
+                self.grow > 0,
+                "Programming error: the number of turns snake grows cannot be negative"
+            );
+            self.grow -= 1;
+        }
+        // Check if we hit snake
+        if self.snake.contains(&(next_x, next_y)) {
+            bail!("Hit the snake!");
+        }
+        self.snake.insert(0, (next_x, next_y));
+        Ok(())
+    }
+
+    pub fn turn_left(&mut self) {
+        self.snake_dir = match self.snake_dir {
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+        };
+    }
+
+    pub fn turn_right(&mut self) {
+        self.snake_dir = match self.snake_dir {
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+            Direction::Up => Direction::Left,
+            Direction::Right => Direction::Up,
+        };
+    }
+
+    pub fn add_thing(&mut self) {
+        let mut rng = rand::thread_rng();
+        loop {
+            let x = rng.gen_range(0..WIDTH);
+            let y = rng.gen_range(0..HEIGHT);
+            if self.snake.contains(&(x, y)) {
+                continue;
+            }
+            if self.things.iter().any(|t| t.1 == x && t.2 == y) {
+                continue;
+            }
+            self.things.push((Thing::Food, x, y));
+            break;
+        }
+    }
+}
+
