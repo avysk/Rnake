9820fd18ebfd33aed2ff624c4c01f6e2ba29c4fc
Alexey Vyskubov <alexey@ocaml.nl>
2023-12-16 16:19:53 +0200

    Use macro to initialize sound structure

--- src/sound.rs
+++ src/sound.rs
@@ -1,8 +1,4 @@
-use sdl2::{
-    audio::{AudioQueue, AudioSpecDesired},
-    sys::SDL_Delay,
-    AudioSubsystem,
-};
+use sdl2::{audio, audio::AudioSpecDesired, sys::SDL_Delay, AudioSubsystem};
 
 pub trait Player {
     fn play(&self, what: &str);
@@ -16,53 +12,51 @@ impl Player for NoSound {
     }
 }
 
-#[repr(align(2))]
-struct Sounds {
-    boom: Vec<u8>,
-    food: Vec<u8>,
-    start: Vec<u8>,
-    wall: Vec<u8>,
-    queue: Option<AudioQueue<i16>>,
-}
+macro_rules! with_sounds {
+    ($($sound:ident)*) => {
+        #[repr(align(2))]
+        struct Sounds {
+            queue: audio::AudioQueue<i16>,
+            $($sound: Vec<u8>),*
+        }
 
-impl Player for Sounds {
-    fn play(&self, what: &str) {
-        let data = match what {
-            "boom" => &self.boom,
-            "food" => &self.food,
-            "start" => &self.start,
-            "wall" => &self.wall,
-            s => panic!("Programming error: unknown sound '{}'", s),
-        };
-        let q = self.queue.as_ref().expect("Should have Audioqueue");
-        q.queue_audio(bytemuck::cast_slice::<u8, i16>(data.as_slice()))
-            .expect("Should be able to queue audio");
-        q.resume();
+        impl Player for Sounds {
+            fn play(&self, what: &str) {
+                let data = match what {
+                    $(stringify!($sound) => &self.$sound),*,
+                    s => panic!("Programming error: unknown sound '{}'", s),
+                };
+                self.queue.queue_audio(bytemuck::cast_slice::<u8, i16>(data.as_slice()))
+                    .expect("Should be able to queue audio");
+                self.queue.resume();
+            }
+        }
+
+        impl Sounds {
+            fn new(system: AudioSubsystem) -> Sounds {
+                let spec = AudioSpecDesired {
+                    freq: None,
+                    channels: Some(1u8),
+                    samples: None,
+                };
+                let queue = system
+                    .open_queue::<i16, Option<&str>>(None, &spec)
+                    .expect("Should be able to open AudioQueue");
+                $(let $sound = Vec::from(*include_bytes!(concat!("sounds/", stringify!($sound), ".wav")));)*
+                Sounds {
+                    queue,
+                    $($sound),*
+                }
+            }
+        }
     }
 }
 
+with_sounds!(boom food start wall);
+
 pub fn new_player(maybe_system: Result<AudioSubsystem, String>) -> Box<dyn Player> {
     if let Ok(system) = maybe_system {
-        let spec = AudioSpecDesired {
-            freq: None,
-            channels: Some(1u8),
-            samples: None,
-        };
-        let queue = match system.open_queue::<i16, Option<&str>>(None, &spec) {
-            Ok(q) => Some(q),
-            _ => panic!("Failed to open audioqueue"),
-        };
-        let boom = Vec::from(*include_bytes!("sounds/boom.wav"));
-        let food = Vec::from(*include_bytes!("sounds/food.wav"));
-        let start = Vec::from(*include_bytes!("sounds/start.wav"));
-        let wall = Vec::from(*include_bytes!("sounds/wall.wav"));
-        Box::new(Sounds {
-            boom,
-            food,
-            start,
-            wall,
-            queue,
-        })
+        Box::new(Sounds::new(system))
     } else {
         Box::new(NoSound {})
     }
@@ -70,11 +64,9 @@ pub fn new_player(maybe_system: Result<AudioSubsystem, String>) -> Box<dyn Playe
 
 impl Drop for Sounds {
     fn drop(&mut self) {
-        if let Some(q) = self.queue.as_ref() {
-            while q.size() > 0 {
-                unsafe {
-                    SDL_Delay(20);
-                }
+        while self.queue.size() > 0 {
+            unsafe {
+                SDL_Delay(20);
             }
         }
     }
